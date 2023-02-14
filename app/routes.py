eec193555bc61d71d3dce247605f50b90e22b98a
JP <j.p.walter@outlook.com>
2023-02-14 13:31:05 -0500

    Began work on user profile pages

--- app/routes.py
+++ app/routes.py
@@ -69,3 +69,16 @@ def register():
         flash("Registration success!")
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
+#<> denote dynamic component, used as function parameter
+#view user page
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    #Queries database for username
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post 1'},
+        {'author': user, 'body': 'Test post 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
