8b3c308f0544e6ae61c45a70592cb996e0b0a753
JP <j.p.walter@outlook.com>
2023-02-22 11:50:03 -0500

    Added explore page, usernames now link to their profile, user icons now display next to post

--- app/routes.py
+++ app/routes.py
@@ -154,3 +154,11 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+#Page to find other users
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    #references index template, reuse because of similarity
+    return render_template('index.html', title='Explore', posts=posts)
